ea8a95926b0241daae91654a8519204f4adbd741
max_k <koval6813@gmail.com>
2021-10-21 09:13:13 +0200

    Setup initial database

--- hangers/models.py
+++ hangers/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class SensorPoint(models.Model):
+    external_temperature = models.DecimalField(max_digits=10, decimal_places=3)
+    body_temperature = models.DecimalField(max_digits=10, decimal_places=3)
+    latitude = models.DecimalField(max_digits=20, decimal_places=15)
+    longitude = models.DecimalField(max_digits=20, decimal_places=15)
+
+
+class Hanger(models.Model):
+    rfid = models.CharField(max_length=36)
+    temperature = models.DecimalField(max_digits=10, decimal_places=3)
+    type = models.CharField(max_length=40)
+
+
+class Status(models.Model):
+    status = models.BooleanField()
+
+
+class CalendarEntry(models.Model):
+    location_name = models.CharField(max_length=100)
+    description = models.TextField()
+    date_time = models.DateTimeField()
+    latitude = models.DecimalField(max_digits=20, decimal_places=15)
+    longitude = models.DecimalField(max_digits=20, decimal_places=15)
+
